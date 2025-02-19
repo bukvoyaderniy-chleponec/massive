fc08f4bc2e9d679a6a0b971187816728dabda6f5
bukvoyaderniy-chleponec <katyakrut2007@gmail.com>
2025-02-19 18:09:12 +0300

    цукпукп
    Microsoft Windows [Version 10.0.19045.5487]
    (c) Корпорация Майкрософт (Microsoft Corporation). Все права защищены.
    
    C:\Users\acer>
    C:\Users\acer>ssh-keygen -t ed25519 -C "katyakrut2007@gmail.com"
    Generating public/private ed25519 key pair.
    Enter file in which to save the key (C:\Users\acer/.ssh/id_ed25519):
    Created directory 'C:\\Users\\acer/.ssh'.
    Enter passphrase (empty for no passphrase):
    Enter same passphrase again:
    Your identification has been saved in C:\Users\acer/.ssh/id_ed25519
    Your public key has been saved in C:\Users\acer/.ssh/id_ed25519.pub
    The key fingerprint is:
    SHA256:9gXBvwgKzb4VAkRbK/nHo7MznsHTO5i0M3/hWLLgngM katyakrut2007@gmail.com
    The key's randomart image is:
    +--[ED25519 256]--+
    |   oo .  ..      |
    |    .+ .  ..     |
    |    ++.   ..     |
    |    .o+.o  ..    |
    |     o.oSo ...   |
    |    E.==oo+..    |
    |     +*B.*..     |
    |      %B+.o      |
    |     o*Boo       |
    +----[SHA256]-----+
    
    C:\Users\acer>eval "$(ssh-agent -s)"
    "eval" не является внутренней или внешней
    командой, исполняемой программой или пакетным файлом.
    
    C:\Users\acer>> Agent pid 59566
    "pid" не является внутренней или внешней
    командой, исполняемой программой или пакетным файлом.
    
    C:\Users\acer>eval "$(ssh-agent - s)"
    "eval" не является внутренней или внешней
    командой, исполняемой программой или пакетным файлом.
    
    C:\Users\acer>$ ssh-add ~/.ssh/id_ed25519
    "$" не является внутренней или внешней
    командой, исполняемой программой или пакетным файлом.
    
    C:\Users\acer>
    
    C:\Users\acer> ssh-add ~/.ssh/id_ed25519
    Error connecting to agent: No such file or directory
    
    C:\Users\acer>Get-Service -Name ssh-agent | Set-Service -StartupType Manual
    "Get-Service" не является внутренней или внешней
    командой, исполняемой программой или пакетным файлом.
    
    C:\Users\acer>Start-Service ssh-agent
    "Start-Service" не является внутренней или внешней
    командой, исполняемой программой или пакетным файлом.
    
    C:\Users\acer>
    
    C:\Users\acer>eval "$(ssh-agent -s)"
    "eval" не является внутренней или внешней
    командой, исполняемой программой или пакетным файлом.
    
    C:\Users\acer>$ cat ~/.ssh/id_ed25519.pub | clip
    "$" не является внутренней или внешней
    командой, исполняемой программой или пакетным файлом.
    
    C:\Users\acer># Copies the contents of the id_ed25519.pub file to your clipboard cat ~/.ssh/id_ed25519.pub | clip
    "#" не является внутренней или внешней
    командой, исполняемой программой или пакетным файлом.
    
    C:\Users\acer> cat ~/.ssh/id_ed25519.pub | clip
    cat: ~/.ssh/id_ed25519.pub: No such file or directory
    
    C:\Users\acer> cat ~/.ssh/id_ed25519.pub | clip
    cat: ~/.ssh/id_ed25519.pub: No such file or directory
    
    C:\Users\acer>cat ~/. ssh/id_rsa
    cat: ~/.: No such file or directory
    cat: ssh/id_rsa: No such file or directory
    
    C:\Users\acer>cat ~/.ssh/id_ed25519.pub | clip
    cat: ~/.ssh/id_ed25519.pub: No such file or directory
    
    C:\Users\acer>
    
    C:\Users\acer>
    
    C:\Users\acer>
    
    C:\Users\acer>ssh-keygen -t ed25519 -C "katyakrut2007@gmail.com"
    Generating public/private ed25519 key pair.
    Enter file in which to save the key (C:\Users\acer/.ssh/id_ed25519):
    C:\Users\acer/.ssh/id_ed25519 already exists.
    Overwrite (y/n)?
    
    C:\Users\acer>
    C:\Users\acer>clip < ~/.ssh/id_ed25519.pub
    Системе не удается найти указанный путь.
    
    C:\Users\acer>

--- завтрак чемпиона чееек.cpp
+++ завтрак чемпиона чееек.cpp
@@ -18,14 +18,14 @@ int main() {
     int size_x = 4, size_y = 5;
     matrica (massiv[0], size_x, size_y);
 
-    //sizeof (massiv)/sizeof (massiv[0]);
+    sizeof (massiv)/sizeof (massiv[0]); // TODO: УБРАТЬ БЕСПОЛЕЗНУЮ СТРОЧКУ
 }
 
 int matrica (int* massiv, size_t size_x, size_t size_y) {
 
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 4; j++)
-            printf ("завтрак чемпиона номер [%d][%d] = %d ", i, j, *(int*)((size_t)massiv + i*size_x*3 +j*4));
+            printf ("завтрак чемпиона номер [%d][%d] = %d ", i, j, *(int*)((size_t)massiv + i*size_x*4 +j*4));
 
         printf ("\n");
     }
